1ff5434a41c01c0e0069ea30c10c81bb9ede9d94
Fahim Shams <pranjolfahim@gmail.com>
2020-04-06 02:02:08 -0500

    Create API endpoint

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Summaries.Data;
+
+namespace Summaries.Controllers
+{
+    [Route("api/[controller]")]
+    public class BooksController:Controller
+    {
+        private IBookService _service;
+        public BooksController(IBookService service)
+        {
+            _service = service;
+        }
+
+        //Create/Add new Book
+        [HttpPost("AddBook")]
+
+        public IActionResult AddBook([FromBody]Book book)
+        {
+            _service.AddBook(book);
+            return Ok("Added");
+        }
+        
+    }
+}
