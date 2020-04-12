6ef694f1df3af6ddc4f0f7216f2d86c0922d5e3e
Fahim Shams <pranjolfahim@gmail.com>
2020-04-12 14:20:48 -0500

    Creating angular components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,11 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BookComponent } from './components/book/book.component';
+import { NewBookComponent } from './components/new-book/new-book.component';
+import { ShowBookComponent } from './components/show-book/show-book.component';
+import { UpdateBookComponent } from './components/update-book/update-book.component';
+import { DeleteBookComponent } from './components/delete-book/delete-book.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    BookComponent,
+    NewBookComponent,
+    ShowBookComponent,
+    UpdateBookComponent,
+    DeleteBookComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
